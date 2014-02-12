238c1dba053aa56fdf82f80e5e2115b30de72c96
mylxsw <mylxsw@126.com>
2014-02-12 20:03:54 +0800

    提供新增和更新页面的方法

--- Demo/models/Page.php
+++ Demo/models/Page.php
@@ -4,5 +4,45 @@ namespace Demo\models;
 
 use Arsenals\Core\Abstracts\Model;
 class Page extends Model {
+    /**
+     * 新增页面
+     * 
+     * @param array $data
+     * @return int
+     */ 
+    public function addPage($data){
+        $entity = array();
+        
+        $entity['title'] = $data['title'];
+        $entity['templates'] = $data['templates'];
+        $entity['creator'] = $data['creator'];
+        $entity['create_date'] = time();
+        $entity['attr'] = $data['attr'];
+        $entity['isvalid'] = 1;
+        
+        return $this->save($entity);
+    }
+    /**
+     * 更新页面
+     * 
+     * @param array $data
+     * @param int id
+     * 
+     * @return void
+     */ 
+    public function updatePage($data, $id){
+        $entity = array();
+        
+        isset($data['title'] && $entity['title'] = $data['title'];
+        isset($data['templates'] && $entity['templates'] = $data['templates'];
+        isset($data['attr'] && $entity['attr'] = $data['attr'];
+        isset($data['isvalid'] && $entity['isvalid'] = $data['isvalid'];
+        
+        $entity['updator'] = $data['creator'];
+        $entity['update_date'] = time();
+        
+        
+        $this->update($entity, array('id'=> intval($id)));
+    }
 
 }
