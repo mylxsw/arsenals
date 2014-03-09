09cea5edb0fb44583abb830c8e66f0ddc5972be4
管宜尧 <mylxsw@126.com>
2014-03-09 16:05:35 +0800

    增加两个异常类

---
+++ Arsenals/Core/Exceptions/AccessDeniedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Arsenals\Core\Exceptions;
+/**
+ * 拒绝访问异常
+ * 
+ * @author 管宜尧<mylxsw@126.com>
+ *
+ */
+class AccessDeniedException extends ArsenalsException {
+}
---
+++ Arsenals/Core/Exceptions/FormInvalidException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Arsenals\Core\Exceptions;
+/**
+ * 表单异常
+ * 
+ * @author 管宜尧<mylxsw@126.com>
+ *
+ */
+class FormInvalidException extends ArsenalsException {
+}
