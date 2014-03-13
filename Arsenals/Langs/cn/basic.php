5de7faa5d201bb21e3309d91b6ae3fa029d999d1
管宜尧 <mylxsw@126.com>
2014-03-13 18:34:28 +0800

    修正翻译错误

--- Arsenals/Langs/cn/basic.php
+++ Arsenals/Langs/cn/basic.php
@@ -8,8 +8,8 @@ return array(
 	'FIELD_MUST_ON_OF'		=>	'字段必须是#{rule}之一!',
 	'FIELD_NOT_VALID_INTEGER'	=>	'字段不是一个合法的整数！',
 	'FIELD_NOT_VALID_FLOAT'	=>	'字段不是一个合法的浮点数！',
-	'FIELD_LEN_LESS'		=>	'字段长度不能小于{$len_min}个字符！',
-	'FIELD_LEN_MORE'		=>	'字段长度不能大于{$len_max}个字符！',
+	'FIELD_LEN_LESS'		=>	'字段长度不能小于#{len_min}个字符！',
+	'FIELD_LEN_MORE'		=>	'字段长度不能大于#{len_max}个字符！',
 	'FIELD_NOT_ID'			=>	'字段不是一个合法的ID！',
 	
 	);
